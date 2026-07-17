a21d9408b5658d0f38f352bdfd6610a841d2823e
ozgesahinbas <ozge.sahinbas58@gmail.com>
2026-07-17 13:26:42 +0300

    Initialize Menu Microservice Spring Boot project

---
+++ Menu-Microservice/src/main/java/com/example/menumicroservice/MenuMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.menumicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MenuMicroserviceApplication.class, args);
+    }
+
+}
---
+++ Menu-Microservice/src/test/java/com/example/menumicroservice/MenuMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.menumicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MenuMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
