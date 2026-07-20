a652a922b4bda8b883133d4164f94b7e76ce102a
ozgesahinbas <ozge.sahinbas58@gmail.com>
2026-07-20 16:03:01 +0300

    Implement create menu endpoint

---
+++ Menu-Microservice/src/main/java/com/example/menumicroservice/controller/MenuController.java
@@ -0,0 +1,20 @@
+package com.example.menumicroservice.controller;
+
+import com.example.menumicroservice.dto.MenuCreateRequest;
+import com.example.menumicroservice.service.MenuServiceImpl;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/menus")
+@RequiredArgsConstructor
+public class MenuController {
+
+    private final MenuServiceImpl menuService;
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public void createMenu(@RequestBody MenuCreateRequest request) {
+        menuService.createMenu(request);
+    }
+}
--- Menu-Microservice/src/main/java/com/example/menumicroservice/dto/MenuCreateRequest.java
+++ Menu-Microservice/src/main/java/com/example/menumicroservice/dto/MenuCreateRequest.java
@@ -2,11 +2,15 @@ package com.example.menumicroservice.dto;
 
 import com.example.menumicroservice.model.MenuStatus;
 import com.example.menumicroservice.model.MenuType;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class MenuCreateRequest {
 
     private String restaurantId;
